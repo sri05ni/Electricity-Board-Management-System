341e09f75315739dd2d7b93b5dc8d7edc2e2162b
Sri <i7srinithi05@gmail.com>
2026-09-23 13:42:46 +0530

    Update EditApplicant.jsx

--- frontend/app/src/components/screens/EditApplicant.jsx
+++ frontend/app/src/components/screens/EditApplicant.jsx
@@ -21,7 +21,7 @@ function EditApplicant() {
     }
   };
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+  // eslint-disable-next-line react-hooks/exhaustive-deps
     useEffect(() => {
       fetchApplicantData();
     }, []);
